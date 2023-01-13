f6349ed2a267b634a98965828c6b081e1159691d
Coen Smulders <coen.smulders@ordina.nl>
2023-01-13 11:02:01 +0100

    clean up code

--- solutions/3_full_customization/main.py
+++ solutions/3_full_customization/main.py
@@ -74,10 +74,16 @@ structlog.configure(
     cache_logger_on_first_use=False,
 )
 
-logger = structlog.get_logger()
-logger.debug("debug message")
-logger.info("info message")
-logger.warning("warn message")
-logger.error("error message")
-logger.critical("critical message")
-logger.login_attempt("user tried to log in on the system", True, user="pythoneer")
+
+def main():
+    logger = structlog.get_logger()
+    logger.debug("debug message")
+    logger.info("info message")
+    logger.warning("warn message")
+    logger.error("error message")
+    logger.critical("critical message")
+    logger.login_attempt("user tried to log in on the system", True, user="pythoneer")
+
+
+if __name__ == "__main__":
+    main()

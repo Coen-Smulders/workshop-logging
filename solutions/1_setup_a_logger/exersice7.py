4d5612cc7c16637e2ee6b999f9e37a2520d73f40
Coen Smulders <coen.smulders@ordina.nl>
2023-01-13 10:37:51 +0100

    spelling

--- solutions/1_setup_a_logger/exersice7.py
+++ solutions/1_setup_a_logger/exersice7.py
@@ -4,6 +4,9 @@ import structlog
 my_path = os.path.abspath(os.path.dirname(__file__))
 path = os.path.join(my_path, "exercise.log")
 print(path)
+
+# Do not use a with construction in production code to pass the file to the logging.
+# Here is it used to ensure the log file is properly closed without boilerplate
 with open(path, "a+") as logfile:
     structlog.configure(
         logger_factory=structlog.PrintLoggerFactory(file=logfile),
